8718153fbec1d9b64078b578e18003580d56ad0a
hussein kazoun <201417104+husseinkazoun@users.noreply.github.com>
2025-12-27 19:18:06 +0200

    Load Squarespace CSS in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -51,6 +51,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${poppins.variable} ${epilogue.variable}`}>
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://assets.squarespace.com/universal/styles-compressed/commerce-2bd20f6587654ec4-min.en-US.css"
+        />
+        <link
+          rel="stylesheet"
+          href="https://static1.squarespace.com/static/versioned-site-css/653146cbbf386b36d5f26d0e/75/5c5a519771c10ba3470d8101/653146cbbf386b36d5f26d16/1717/site.css"
+        />
+        <link
+          rel="stylesheet"
+          href="https://static1.squarespace.com/static/vta/5c5a519771c10ba3470d8101/versioned-assets/1766184538619-72W2T78VKF7EGEIPSK0V/static.css"
+        />
+      </head>
       <body className={squarespaceBodyClass}>
         <div id="siteWrapper" className="clearfix site-wrapper">
           <SquarespaceHeader />
